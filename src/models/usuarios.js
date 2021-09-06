1b69af28bea5968378e35c72626f2026e519e103
Kamila Silva <kamillasilva140@gmail.com>
2021-09-06 20:03:00 -0300

    inseri model de usuarios

---
+++ src/models/usuarios.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const usuariosSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    nome: {
+        type: String,
+        required: true
+    },
+    endereço: {
+        type: String,
+        required: true,
+    }, 
+    telefone:{
+        type: String,
+        required: true
+    },
+    pet: {
+        type: String,
+        required: true
+    },
+    pontodeColeta: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'coleta'
+    },
+    cadastroEm: {
+        type:Date,
+        required: true,
+        default: new Date
+    }
+})
+
+module.exports = mongoose.model("usuarios", usuariosSchema)
