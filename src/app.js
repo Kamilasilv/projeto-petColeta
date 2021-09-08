36021c799b83705ff0c905b242f9ef4623646570
Kamila Silva <kamillasilva140@gmail.com>
2021-09-08 16:43:06 -0300

    inclui rota adm

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+require('dotenv-safe').config()
 const index = require("../src/routes/index");
 const coletaRoutes = require("../src/routes/coleta.routes")
 const usuariosRoutes = require("../src/routes/usuarios.routes")
+const admRoutes = require("../src/routes/adm.routes")
 
 
 const db = require("../src/data/database")
@@ -18,5 +19,6 @@ app.use(express.json())
 app.use("/",index )
 app.use("/coletas",coletaRoutes)
 app.use("/usuarios",usuariosRoutes)
+app.use("/adm", admRoutes)
 
 module.exports = app
