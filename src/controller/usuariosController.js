59f181ae55c2263d6c36f99edb0bf7db10bcee71
Kamila Silva <kamillasilva140@gmail.com>
2021-09-08 22:57:16 -0300

    rota get collect teste2

--- src/controller/usuariosController.js
+++ src/controller/usuariosController.js
@@ -8,8 +8,8 @@ const getAllPeople = async (req, res) => {
 
 const getAllCollect = async (req, res) => {
     const nomeColeta = req.query.nome
-    const usuarios = await Usuarios.find()
-    const filteredUser = usuarios.filter( usuario => usuario.coleta.nome == nomeColeta).populate('coleta')
+    const usuarios = await Usuarios.find(nomeColeta).populate('coleta')
+    //const filteredUser = usuarios.filter( usuario => usuario.coleta.nome == nomeColeta).populate('coleta')
     res.status(200).json(filteredUser)
 }
 
