b39eaa4775170a4a6a1d88f9e880b391005e9158
Kamila Silva <kamillasilva140@gmail.com>
2021-09-10 15:49:30 -0300

    Apresentação projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res)=>{
     res.status(200).send({
-        "mensagem": "Bem vindo ao Projeto Pet-Coleta",
+        "mensagem": "Bem vindo ao Projeto Pet-Coleta {reprograma}",
         "title":  "Pet-Coleta",
         "version": "1.0.0"
     })
